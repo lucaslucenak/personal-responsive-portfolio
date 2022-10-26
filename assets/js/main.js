f0c8e8282fc0e165a7362a90c64052a200130833
Lucas Lucena <lucas.lucenak@gmail.com>
2022-10-26 07:09:40 -0300

    Add Contact Me

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,7 +89,7 @@ modalCloses.forEach((modalClose) => {
 })
 
 /* Portfolio Swiper */
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
     navigation: {
@@ -100,4 +100,20 @@ let swiper = new Swiper('.portfolio__container', {
         el: '.swiper-pagination',
         clickable: true,
     },
+});
+
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        }
+    }
 });
